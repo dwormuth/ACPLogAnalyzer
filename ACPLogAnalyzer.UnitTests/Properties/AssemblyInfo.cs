b64a5b168e427e107cc1dd357247dbfd0f4f4828
David W. Wormuth <dwormuth@post.harvard.edu>
2018-04-04 17:48:16 -0400

    Adding testing attempt
    Doesn't work at present

---
+++ ACPLogAnalyzer.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACPLogAnalyzer.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACPLogAnalyzer.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4257819-2b63-47a7-af49-b997c5ca0faa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ACPLogAnalyzer.UnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACPLogAnalyzer;
+
+namespace ACPLogAnalyzer.UnitTests
+{
+
+    [TestClass]
+    public class ACPLogAnalyzerTests
+    {
+        [TestMethod]
+        public void ParseRepeat_CorrectString_True()
+        {
+            static string mytest = "starting target reepeat";
+            static List<string> testString = new List<string>(mytest);
+
+            Log log = new Log("foo", testString);
+
+            //LineLower = "starting target reepeat";
+            Assert.IsTrue(ACPLogAnalyzer.Log.ParseRepeat());
+            //Assert.IsTrue(true);
+
+        }
+    }
+}
