76caadc005d14992f39b7d526feced0481b26ca5
Igor Dulevich <gamba69@gmail.com>
2018-10-26 20:43:02 +0300

    Prepare release

--- ACPLogAnalyzer/Properties/AssemblyInfo.cs
+++ ACPLogAnalyzer/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Windows;
 [assembly: AssemblyTitle("ACPLogAnalyzer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Russell Archer/David Wormuth")]
+[assembly: AssemblyCompany("Russell Archer/David Wormuth/Igor Dulevich")]
 [assembly: AssemblyProduct("ACP Log Analyzer")]
-[assembly: AssemblyCopyright("Copyright © Russell Archer 2011, 2012, 2013, David Wormuth 2017-2018")]
+[assembly: AssemblyCopyright("Copyright © Russell Archer 2011, 2012, 2013, David Wormuth 2017-2018, Igor Dulevich 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,5 +25,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("1.3.6")]
-[assembly: AssemblyFileVersion("1.3.6.0")]
+[assembly: AssemblyVersion("1.3.7")]
+[assembly: AssemblyFileVersion("1.3.7.0")]
